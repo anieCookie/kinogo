f08b3fd0ebe62951975a46720385a6d79700e416
anieCookie <aniekorot26@gmail.com>
2025-02-26 15:53:52 +0500

    Update Keyboards.py

--- Keyboards.py
+++ Keyboards.py
@@ -10,7 +10,7 @@ first_button_kb = ReplyKeyboardMarkup(keyboard=[
 main_inlines_kb = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Искать фильм по тегу #️⃣", callback_data="tag")],
     [InlineKeyboardButton(text="Искать фильм по описанию 📝", callback_data="movie")],
-    [InlineKeyboardButton(text="Персональные рекомендации 💁‍♂️", callback_data="recommend",)],
+    [InlineKeyboardButton(text="Персональные рекомендации 💁‍♂️", callback_data="recommend")],
     ])
 
 
@@ -22,35 +22,10 @@ tags = InlineKeyboardMarkup(inline_keyboard=[
 
 #библиотека//////////////////////////////////////////////////////////////////////////////////
 lib = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="А.. - Д.. ", callback_data="b")],
-    [InlineKeyboardButton(text="Е.. - Р.. ", callback_data="k")],
-    [InlineKeyboardButton(text="С.. - Я.. ", callback_data="f")],
+    [InlineKeyboardButton(text="Жанры ", callback_data="genre")],
+    [InlineKeyboardButton(text="Теги ", callback_data="tags")]
     ])
 
-
-b = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Биография ", callback_data="bio")],
-    [InlineKeyboardButton(text="Боевик ", callback_data="boevik")],
-    [InlineKeyboardButton(text="Вестерн ", callback_data="west")],
-    [InlineKeyboardButton(text="Детектив ", callback_data="detective")],
-    [InlineKeyboardButton(text="Драма ", callback_data="drama")]
-    ])
-
-k = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Комедия ", callback_data="comedy")],
-    [InlineKeyboardButton(text="Мелодрама ", callback_data="movie")],
-    [InlineKeyboardButton(text="Мистика ", callback_data="mistic")],
-    [InlineKeyboardButton(text="Приключение ", callback_data="adventure")],
-    [InlineKeyboardButton(text="Ромком ", callback_data="romcom")]
-    ])
-
-f = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Трагикомедия ", callback_data="tragedy")],
-    [InlineKeyboardButton(text="Триллер ", callback_data="triller")],
-    [InlineKeyboardButton(text="Ужасы ", callback_data="horror")],
-    [InlineKeyboardButton(text="Фантастика ", callback_data="fantastic")],
-    [InlineKeyboardButton(text="Фэнтези ", callback_data="fantasy")]
-    ])
 
 #реки////////////////////////////////////////////////////////////////////////////////////////
 recommend = InlineKeyboardMarkup(inline_keyboard=[
